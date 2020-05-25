8211337d34c5a9be7ba60c7b16631beb3f899fee
tejaswinirajput <64858703+tejaswinirajput@users.noreply.github.com>
2020-05-25 21:15:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def init():
     global model,graph
     # load the pre-trained Keras model
     model = load_model('my_sentimeter_model.h5')
-    model.compile(loss='binary_crossentropy' optimizer='adam' metrics= 'accuracy'
-    model.load_weights('my_sentimeter_weights.h5)
+    model.compile(loss='binary_crossentropy',optimizer='adam')
+    model.load_weights('my_sentimeter_weights.h5')
     graph = tf.get_default_graph()
 
 #########################Code for Sentiment Analysis
@@ -34,7 +34,7 @@ def sent_anly_prediction():
     if request.method=='POST':
         text = request.form['text']
         sentiment = ''
-        max_review_length = 500
+        max_review_length = 2697
         word_to_id = imdb.get_word_index()
         strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
         text = text.lower().replace("<br />", " ")
@@ -42,7 +42,7 @@ def sent_anly_prediction():
 
         words = text.split() #split string into a list
         x_test = [[word_to_id[word] if (word in word_to_id and word_to_id[word]<=20000) else 0 for word in words]]
-        x_test = sequence.pad_sequences(x_test, maxlen=500) # Should be same which you used for training data
+        x_test = sequence.pad_sequences(x_test, maxlen=2697) # Should be same which you used for training data
         vector = np.array([x_test.flatten()])
         with graph.as_default():
             probability = model.predict(array([vector][0]))[0][0]
